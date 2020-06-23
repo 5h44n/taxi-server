2eb1c75ebf402ffeeb6513747b0bf6d99181a7ae
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-06-22 20:03:05 -0700

    Add websocket consumer that can broadcast messages to a group

--- taxi/taxi/routing.py
+++ taxi/taxi/routing.py
@@ -3,10 +3,13 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 
+from taxi.middleware import TokenAuthMiddlewareStack
 from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter([
-        path('taxi/', TaxiConsumer),
-    ]),
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer),
+        ]),
+    ),
 })
--- taxi/trips/consumers.py
+++ taxi/trips/consumers.py
@@ -1,13 +1,37 @@
 # trips/consumers.py
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from trips.models import Trip
+
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
+    groups = ['test']
+
     async def connect(self):
-        await self.accept()
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            await self.channel_layer.group_add(
+                group='test',
+                channel=self.channel_name
+            )
+            await self.accept()
+
 
     async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
         await super().disconnect(code)
+
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
@@ -16,3 +40,7 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 'type': message_type,
                 'data': content.get('data'),
             })
+
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
